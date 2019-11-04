2ec639f51ef9cfe1f8653a30a78f1d7d757547ba
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-04 20:01:02 +0000

    gif

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-    res.send('<iframe src="https://giphy.com/embed/Efpfk11XWXwTSKqgV2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
+    res.send('<iframe src="https://media.tenor.com/images/300b6f2a999105591f780526794e5129/tenor.gif" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
 });
 
 app.listen(PORT, function () {
